eea35506902b7125136f57c67713ca904019cf7c
akittles <151872598+akittles@users.noreply.github.com>
2024-08-30 14:04:37 -0400

    Update main_page_steps.py

--- main_page_steps.py
+++ main_page_steps.py
@@ -11,31 +11,25 @@ def open_target(context):
 # search button
 @when('Search for {product}')
 def search_product(context, product):
+    # find search field and enter text
     context.driver.find_element(By.ID, 'search').send_keys(product)
+    # click search
     context.driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
-    sleep(6)
+    # wait for the page with search results to load
+    sleep(10)
 
 
+@when('Click on Cart icon')
+def click_cart(context):
+    context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/CartLink']").click()
 
-#-------------DO NOT DO THIS!!
-# # find empty cart (THIS IS AN EXAMPLE OF HAVING 2 OF THE SAME STEPS)
-# @when('Click on cart icon')
-# def click_cart_icon(context):
-#     context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/CartLink']").click()
-#     sleep(6)
-#     context.driver.find_element(By.CSS_SELECTOR, "[data-test='boxEmptyMsg']")
-
-
-# sign in
-@when('Click on sign in')
-def first_signin(context):
-    context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/AccountLink']").click()
-
-
-@when('Click on window sign in')
-def window_signin(context):
-    context.driver.find_element(By.CSS_SELECTOR, "[data-test='accountNav-signIn']").click()
-    sleep(6)
+    #-------------DO NOT DO THIS!!---------------
+    # # find empty cart (THIS IS AN EXAMPLE OF HAVING 2 OF THE SAME STEPS)
+    # @when('Click on cart icon')
+    # def click_cart_icon(context):
+    #     context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/CartLink']").click()
+    #     sleep(6)
+    #     context.driver.find_element(By.CSS_SELECTOR, "[data-test='boxEmptyMsg']")
 
 
 @then('Verify header in shown')
@@ -48,6 +42,14 @@ def verify_header_link_amount(context, number):
     number = int(number) # turns string "6" into integer 6
     links = context.driver.find_elements(By.CSS_SELECTOR, "[id*= 'utilityNav']")
     assert len(links) == number, f'Expected {number} links, but got {len(links)}'
+    # Make sure to always assert len() for multiple elements as shown above
+    # because .find_elements() function never fails.
+    # This code with incorrect locator will always pass:
+    # context.driver.find_elements(By.CSS_SELECTOR, "[id*='utilityNav2613542']")
+
+
+    # ************************     NOTES        ***************************
+
 
     # -----if just trying to count or verify the amount of elements on a page, do this
     # number = int(number)  # turns string "6" into integer 6
@@ -57,19 +59,4 @@ def verify_header_link_amount(context, number):
     # -------------------only have this line of code below-------
     # links = context.driver.find_elements(By.CSS_SELECTOR, "[id*= 'utilityNav']")
     # assert len(links) > 0 #ALWAYS USE ASSERT TO VERIFY ELEMENTS ARE THERE WHEN USING find_elementS
-#
-# ------------------------------------------------------------------------
-# ------POSSIBLY DELETE BELOW
-# @then('Verify your cart is empty message is shown')
-# def verify_cart_is_empty(context):
-#     expected_text = 'Your cart is empty'
-#     actual_element = context.driver.find_element(By.CSS_SELECTOR, "[data-test='boxEmptyMsg']")
-#     actual_text = actual_element.text
-#     print(actual_text)
-#     assert expected_text in actual_text, f'Expected text {expected_text} is not in actual text {actual_text}'
-#     #
-#     print('Test case passed')
-#
-# ------------------------------------------------------------------------
-
 
