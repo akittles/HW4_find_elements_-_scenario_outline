1b7a144db4eeb76d6ca88b6b579e65852c2e9e57
akittles <151872598+akittles@users.noreply.github.com>
2024-08-30 13:50:08 -0400

    Update search_results_steps.py

--- search_results_steps.py
+++ search_results_steps.py
@@ -1,17 +1,19 @@
 from selenium.webdriver.common.by import By
-from behave import given, when, then
+from selenium.webdriver.support import expected_conditions as EC
+from behave import then, when
 from time import sleep
 
 ADD_TO_CART_BTN = (By.CSS_SELECTOR, "[id*='addToCartButton']")
-SIDE_NAV_PRODUCT_NAME = (By.CSS_SELECTOR, "[data-test='content-wrapper'] a h4")
+SIDE_NAV_PRODUCT_NAME = (By.CSS_SELECTOR, "[data-test='content-wrapper'] h4")
 SIDE_NAV_ADD_TO_CART_BTN = (By.CSS_SELECTOR, "[data-test='content-wrapper'] [id*='addToCartButton']")
+# [data-test='@web/AddToCart/FulfillmentSection']
 
 
 @when('Click on Add to Cart button')
 def click_add_to_cart(context):
-    context.driver.find_element(*ADD_TO_CART_BTN).click()  #always clicks on 1st add to cart btn
-    # context.driver.find_elements(By.CSS_SELECTOR, "[id*='addToCartButton']")[0].click
-    sleep(4)
+    context.driver.find_element(*ADD_TO_CART_BTN).click()  # always clicks on 1st Add to cart btn
+    # context.driver.find_elements(By.CSS_SELECTOR, "[id*='addToCartButton']")[0].click()
+    context.driver.wait.until(EC.visibility_of_element_located(SIDE_NAV_PRODUCT_NAME))
 
 
 @when('Store product name')
@@ -33,14 +35,14 @@ def verify_search_worked(context, product):
         f'Expected text {product} is not in actual text {actual_text}'
 
 
-@when('Click on cart button')
-def click_cart(context):
-    context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/CartLink']").click()
-
-
-@then('Verify correct search results url opens for {product}')
-def verify_url(context, product):
+@then('Verify correct search results url opens for {expected_product}') # changed product to expected_product
+def verify_url(context, expected_product):
     url = context.driver.current_url
-    assert product in context.driver.current_url, \
-        f'Expected {product} not in url {url}'
-#
+    assert expected_product in context.driver.current_url, \
+        f'Expected {expected_product} not in url {url}'
+
+#     ********************   POSSIBLY DELETE         *************
+##
+# @when('Click on cart button')
+# def click_cart(context):
+#     context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/CartLink']").click()
